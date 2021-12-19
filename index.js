ff247dfd1550bcd6a8d0c59bb2448eee10ccc250
weaksou <31419788+weaksou@users.noreply.github.com>
2021-12-19 21:34:29 +0100

    second commit

--- index.js
+++ index.js
@@ -2,14 +2,34 @@ const calcButton = document.getElementById("calc");
 const height = document.getElementById("height");
 const width = document.getElementById("width");
 const number = document.getElementById("number");
+const customNumber = document.getElementById("customNumber");
 const resultBox = document.getElementById("result");
+const currentRange = document.getElementById("currentRange");
+
+
+number.addEventListener("change", (event) => {
+    event.preventDefault();
+
+    const h = Number(height.value);
+    const w = Number(width.value);
+    const n = Number(event.target.value);
+    currentRange.textContent = n;
+
+    const result = calcRatio(h, w, n);
+
+    resultBox.innerHTML = `
+        <div>Height: ${result[0]} </div>
+        <div>Width: ${result[1]} </div>
+    `;
+})
+
 
 calcButton.addEventListener("click", (e) => {
     e.preventDefault();
 
     const h = Number(height.value);
     const w = Number(width.value);
-    const n = Number(number.value);
+    const n = Number(customNumber.value);
 
     const result = calcRatio(h, w, n);
 
@@ -23,8 +43,16 @@ calcButton.addEventListener("click", (e) => {
 //
 
 function calcRatio(h, w, num){
-    const ratioValue = (h / w);
 
+    if(!h){
+        return ["please specify a height", ""];
+    }
+    if(!w){
+        return ["", "please specify a width"];
+    }
+
+    const ratioValue = (h / w);
+    
     let heightValue = h;
     let widthValue = w;
 
