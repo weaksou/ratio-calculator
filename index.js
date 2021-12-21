40ff7cb8eaf41d577a02fa7d1b3f32b80902fa01
weaksou <31419788+weaksou@users.noreply.github.com>
2021-12-21 18:36:31 +0100

    v

--- index.js
+++ index.js
@@ -1,64 +1,27 @@
-const calcButton = document.getElementById("calc");
 const height = document.getElementById("height");
 const width = document.getElementById("width");
-const number = document.getElementById("number");
-const customNumber = document.getElementById("customNumber");
-const resultBox = document.getElementById("result");
-const currentRange = document.getElementById("currentRange");
 
+let beforeChangeHeight = 0;
+let beforeChangeWidth = 0;
 
-number.addEventListener("change", (event) => {
-    event.preventDefault();
+height.addEventListener("change", () => {
 
-    const h = Number(height.value);
-    const w = Number(width.value);
-    const n = Number(event.target.value);
-    currentRange.textContent = n;
+    if(Number(height.value) !== beforeChangeHeight){
+        const res = calcRatio(Number(height.value), Number(width.value), (Number(height.value) - beforeChangeHeight));
+        beforeChangeHeight = Number(height.value);
+        
+        height.value = res[0];
+        width.value = res[1];
+    }
 
-    const result = calcRatio(h, w, n);
-
-    resultBox.innerHTML = `
-        <div>Height: ${result[0]} </div>
-        <div>Width: ${result[1]} </div>
-    `;
 })
 
 
-calcButton.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const h = Number(height.value);
-    const w = Number(width.value);
-    const n = Number(customNumber.value);
-
-    const result = calcRatio(h, w, n);
-
-    resultBox.innerHTML = `
-        <div>Height: ${result[0]} </div>
-        <div>Width: ${result[1]} </div>
-    `;
-});
-
-
-//
-
 function calcRatio(h, w, num){
+    const ratio = (h / w);
 
-    if(!h){
-        return ["please specify a height", ""];
-    }
-    if(!w){
-        return ["", "please specify a width"];
-    }
-
-    const ratioValue = (h / w);
+    h = h + (ratio * num);
+    w = w + (ratio * num); 
     
-    let heightValue = h;
-    let widthValue = w;
-
-    heightValue = heightValue + (ratioValue * num);
-    widthValue = widthValue + (ratioValue * num); 
-    
-    
-    return [heightValue, widthValue];
+    return [h, w];
 }
